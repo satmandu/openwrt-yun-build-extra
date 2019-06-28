543e0473374d920651089b28fec9af642e11420e
Satadru Pramanik <satadru@umich.edu>
2019-06-28 18:52:17 -0400

    Updated configs

--- rebuild.sh
+++ rebuild.sh
@@ -9,20 +9,20 @@ rm  -f ./package/feeds/packages/node
 rm  -f ./package/feeds/packages/node-*
 
 #INSTALL CUSTOM NODE PACKAGES
-./scripts/feeds install -a -p node
+#./scripts/feeds install -a -p node
 
 #DELETE OPENWRT MRAA PACKAGES
 rm -f ./package/feeds/packages/libmraa
 rm -f ./package/feeds/packages/libupm
 
 #INSTALL CUSTOM MRAA PACKAGES
-./scripts/feeds install -a -p inteliot
+#./scripts/feeds install -a -p inteliot
 
 #DELETE OPENWRT PAHO-MQTT PACKAGE
 rm -f ./package/feeds/packages/python-paho-mqtt
 
 #INSTALL CUSTOM PACKAGES
-./scripts/feeds install -a -p custom
+#./scripts/feeds install -a -p custom
 
 cp openwrt-yun-lininoos.diffconfig .config
 
